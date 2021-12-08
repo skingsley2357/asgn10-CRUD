a31b53ff581c6e2dcf21e6d518d2b0fe17b8130a
Scott Kingsley <scottakingsley@students.abtech.edu>
2021-12-08 16:54:34 -0500

    added code for new.php and create.php.

--- private/query_functions.php
+++ private/query_functions.php
@@ -8,3 +8,35 @@ function find_all_salamanders() {
     confirm_result_set($result);
     return $result;
 }
+
+function find_salamander_by_id($id) {
+    global $db;
+    $sql = "SELECT * FROM salamander ";
+    $sql .= "WHERE id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $salamander = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $salamander;
+}
+
+function insert_subject ($salamanderName, $salamanderHabitat, $salamanderDescription) {
+    global $db;
+
+    $sql = "INSERT INTO subjects ";
+    $sql .= "(menu_name, position, visible) ";
+    $sql .= "VALUES (";
+    $sql .= "'" . $salamanderName . "',";
+    $sql .= "'" . $salamanderHabitat . "',";
+    $sql .= "'" . $salamanderDescription . "'";
+    $sql .= ")";
+    $result = mysqli_query($db, $sql);
+  
+    if($result) {
+      return true; 
+    } else {
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+  }
--- public/salamanders/show.php
+++ public/salamanders/show.php
@@ -4,12 +4,33 @@ require_once('../../private/initialize.php');
 // $id = isset($_GET['id']) ? $_GET['id'] : '1';
 $id = $_GET['id'] ?? '1'; // PHP > 7.0
 
+$salamander = find_salamander_by_id($id);
+
 $page_title = 'View Salamander';
 include(SHARED_PATH . '/salamanderHeader.php'); 
 ?>
 
 <a href="<?= url_for('/salamanders/index.php'); ?>">&laquo; Back to List</a>
 
-Page ID: <?= h($id); ?>
+<h1>Subject: <?= h($salamander['name']); ?></h1>
+
+    <div class="attributes">
+      <dl>
+        <dt>ID</dt>
+        <dd><?= h($salamander['id']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Name</dt>
+        <dd><?= h($salamander['name']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Habitat</dt>
+        <dd><?= h($salamander['habitat']); ?></dd>
+      </dl>
+      <dl>
+        <dt>Description</dt>
+        <dd><?= h($salamander['description']); ?></dd>
+      </dl>
+    </div>
 
 <?php include(SHARED_PATH . '/salamanderFooter.php'); ?>
