ac892651fa79dbbffdd6978d04ae6545f6a1036e
Scott Kingsley <scottakingsley@students.abtech.edu>
2021-12-08 16:34:07 -0500

    added code to make show.php work.

--- private/query_functions.php
+++ private/query_functions.php
@@ -8,3 +8,15 @@ function find_all_salamanders() {
     confirm_result_set($result);
     return $result;
 }
+
+function find_salamander_by_id($id) {
+    global $db;
+    $sql = "SELECT * FROM salamander ";
+    $sql .= "WHERE id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $salamander = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $salamander;
+}
+?>
